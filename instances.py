3c386fd9435c0f4f8d607b1de464feeeb7aea1aa
Michael Madden <mikemadden42@icloud.com>
2015-02-03 21:48:57 -0600

    Added instances.py

---
+++ instances.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import boto.ec2
+import sys
+
+
+def instances():
+    print 'boto %s on %s.' % (boto.Version, sys.platform)
+
+    conn = boto.ec2.connect_to_region("us-east-1")
+    reservations = conn.get_all_reservations()
+
+    for r in reservations:
+        instances = r.instances
+        for i in instances:
+            #print dir(i)
+            print i.id
+            print i.instance_type
+            print i.ip_address
+            print i.key_name
+            print i.launch_time
+            print i.region.name
+            print i.state
+
+
+if __name__ == '__main__':
+    instances()
